a940993734857d5cdd118728091073ee2fa93188
nguyen duy cao <namduycao@gmail.com>
2023-12-18 21:52:06 +0700

    project android studio tuan2

---
+++ app/src/main/java/com/example/learn3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.learn3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=(WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/Band/index.html");
+    }
+}
